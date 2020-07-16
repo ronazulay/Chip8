0adc203706ad7fdc0912ea212d93289d2a8dd3d5
Ron Azulay <ron.azulay@gmail.com>
2020-07-16 16:11:47 +0200

    Added missing files in Chip8.Tests.

---
+++ Chip8.Tests/Vm/MockWindow.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chip8.Tests
+{
+    class MockWindow : IWindow
+    {
+        public void Beep()
+        {
+        }
+
+        public void ProcessEvents()
+        {
+        }
+
+        public void Render()
+        {
+        }
+    }
+}
