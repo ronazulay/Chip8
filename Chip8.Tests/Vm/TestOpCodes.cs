52e8730df20de96a41f98d48ff74ddf28125c4b3
Ron Azulay <ron.azulay@gmail.com>
2020-07-16 21:05:34 +0200

    Added a couple of more OpCode tests.

--- Chip8.Tests/Vm/TestOpCodes.cs
+++ Chip8.Tests/Vm/TestOpCodes.cs
@@ -27,26 +27,73 @@ namespace Chip8.Tests
         public void Test_OpCode00EE_ShouldSetPCTo0x202()
         {
             var vm = Vm.NewVm(new MockWindow(), new byte[] {
-                0x22, 0x04,  // 0x2204 - Call subroutine at 0x204
+                0x22, 0x04,  // Call subroutine at 0x204
                 0x13, 0x37,  // Execution should continue here at 0x202
-                0x00, 0xEE   // 0x00EE - Return to caller
+                0x00, 0xEE   // Return to caller
 
             });
-            vm.EmulateCycle();
-            vm.EmulateCycle();
+            vm.EmulateCycles(2);
 
             Assert.AreEqual(vm.PC, 0x202);
         }
 
+        [Test]
         public void Test_OpCode1NNN_ShouldSetPCTo0x123()
         {
             var vm = Vm.NewVm(new MockWindow(), new byte[] {
-                0x11, 0x23,  // 0x1123 - Jump to address 0x123
+                0x11, 0x23,  // Jump to address 0x123
             });
-            vm.EmulateCycle();
             vm.EmulateCycle();
 
             Assert.AreEqual(vm.PC, 0x123);
+        }
+
+        [Test]
+        public void Test_OpCode3XNN_ShouldSetPCTo0x206()
+        {
+            var vm = Vm.NewVm(new MockWindow(), new byte[] {
+                0x60, 0x11,  // Set V0 to 0x11.
+                0x30, 0x11   // Should skip the next instruction
+            });
+            vm.EmulateCycles(2);
+
+            Assert.AreEqual(vm.PC, 0x206);
+        }
+
+        [Test]
+        public void Test_OpCode4XNN_ShouldSetPCTo0x206()
+        {
+            var vm = Vm.NewVm(new MockWindow(), new byte[] {
+                0x60, 0x11,  // Set V0 to 0x11.
+                0x40, 0x12   // Should skip the next instruction
+            });
+            vm.EmulateCycles(2);
+
+            Assert.AreEqual(vm.PC, 0x206);
+        }
+
+        [Test]
+        public void Test_OpCode5XY0_ShouldSetPCTo0x208()
+        {
+            var vm = Vm.NewVm(new MockWindow(), new byte[] {
+                0x60, 0x11,  // Set V0 to 0x11.
+                0x61, 0x11,  // Set V1 to 0x11.
+                0x50, 0x10   // Should skip the next instruction
+            });
+            vm.EmulateCycles(3);
+
+            Assert.AreEqual(vm.PC, 0x208);
+        }
+
+        [Test]
+        public void Test_OpCode6XNN_ShouldSetV0To0xAA()
+        {
+            var vm = Vm.NewVm(new MockWindow(), new byte[] {
+                0x60, 0xAA,  // Set V0 to 0xAA.
+            });
+            vm.EmulateCycle();
+
+            Assert.AreEqual(vm.V[0], 0xAA);
         }
     }
 }
